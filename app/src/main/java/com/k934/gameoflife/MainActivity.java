4e7077baf213346aabcc61ad9116596aeee9c2e8
kolya934 <protas.nikolay@gmail.com>
2015-04-21 00:31:45 +0300

    Добавлен обработчик нажатия кнопки

--- app/src/main/java/com/k934/gameoflife/MainActivity.java
+++ app/src/main/java/com/k934/gameoflife/MainActivity.java
@@ -1,17 +1,35 @@
 package com.k934.gameoflife;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+    }
+
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.new_button:
+                    Toast.makeText(this, "Скоро эта кнопка будет запускать игру!:)", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+
     }
 
 
@@ -40,4 +58,3 @@ public class MainActivity extends ActionBarActivity {
     }
 
     */
-}
